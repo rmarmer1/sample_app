bce6a46e37f2cd776f7d9eec56e1147ea770fae2
rmarmer1 <rmarmer1@yahoo.com>
2013-07-09 02:44:21 -0400

    Add StaticPages code from Chapter 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '159c8892cf661c4c18b2d5575108a8dd8bf3b9bcb2051fccb5e0a1440fb62ca758862be2d653416e46d8e019a61b728d1916b3ab8698ff6e9ba054f13e04c52e'
